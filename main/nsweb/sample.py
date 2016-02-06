d6d1cd6fd5a82661a07de213ecca7fa79f475995
Joshua Schnarr <joshua@jpserve.net>
2016-02-06 10:58:50 +0000

    add nsweb sample ui

---
+++ main/nsweb/sample.py
@@ -0,0 +1,77 @@
+#!python2
+
+import os
+import time
+
+from flask import Flask, render_template, jsonify, request, make_response
+from flask.ext.assets import Environment, Bundle
+
+import nsweb
+
+app = Flask(__name__)
+
+assets = Environment(app)
+version = 1
+app.debug = True
+app.config['ASSETS_DEBUG'] = app.debug
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['UGLIFYJS_BIN'] = '/usr/local/bin/uglifyjs'
+app.config['UGLIFYJS_EXTRA_ARGS'] = ['--comments', '/(MIT|license)/']
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+from jsonschema import validate, ValidationError
+import json
+schema_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static', 'schema', 'ns.json') 
+with open(schema_filename) as jsonschema:
+    neoschema = json.loads(jsonschema.read())
+
+js_files = "modernizr.min.js jquery{min:s}.js bootstrap{min:s}.js ie10-viewport-bug-workaround.js jquery.bootpag.min.js nsweb.js"
+js_files = js_files.format(min="" if app.debug else ".min").split()
+js_files = [os.path.join("js", f) for f in js_files]
+css_files = "bootstrap.readable.min.css nsweb.css".split()
+css_files = [os.path.join("css", f) for f in css_files]
+js = Bundle(*js_files, filters='uglifyjs', output='js/min.js')
+css = Bundle(*css_files, output='css/min.css')
+assets.register('js_all', js)
+assets.register('css_all', css)
+
+class InputError(Exception):
+    pass
+
+def run_query(query):
+    engine = nsweb.NeoEngine()
+    return engine.match(query['params'][0])
+
+@app.route("/")
+def route():
+    return render_template('sample.html', debug=app.debug, version=version)
+
+@app.route("/Search/", methods=['POST'])
+def network():
+    try:
+        query = request.get_json()
+        validate(query, neoschema)
+        result = run_query(query)
+    except InputError as e:
+        return jsonify(errors=[unicode(e)])
+    except RuntimeError as e:
+        return jsonify(errors=[unicode(e)])
+    except ValidationError as e:
+        return jsonify(errors=[u"Input error in '{:s}': {:s}.".format(e.path[0], e.message)])
+
+    response = make_response(result)
+    response.mimetype = 'application/json'
+    return response
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
---
+++ main/nsweb/static/js/nsweb.js
@@ -0,0 +1,86 @@
+var lastQuery = null;
+var loading = false;
+
+function ParseQuery() {
+    var searchData = {};
+    searchData['version'] = 2;
+    searchData['first'] = 0;
+    searchData['count'] = 10;
+    searchData['params'] = [];
+    searchData['params'].push($('input#textInput0').val());
+    return searchData;
+}
+
+function LoadResults(d) {
+    loading = true;
+    var bookResults = "";
+    $.each(d.volumes, function (i, vol) {
+        if (vol.count > 0) {
+            bookResults += '<label class="btn btn-default btn-badged-checkbox">';
+            bookResults += '<input type="checkbox"><span>' + vol.name + '</span>';
+            bookResults += '<span class="badge">' + vol.count + '</span></label>\n';
+        }
+    });
+    $('div#resultsByBook').html(bookResults); 
+    $('#pageSelection').bootpag({
+        total: (d.count + lastQuery.count - 1) / lastQuery.count,
+        page: lastQuery.first / lastQuery.count + 1,
+        maxVisible: 10
+    }).off("page").on("page", function(event, page) {
+        if (!loading) {
+            var newQuery = $.extend({}, lastQuery);
+            newQuery.first = (page - 1) * newQuery.count;
+            RunQuery(newQuery);
+        }
+    });
+    loading = false;
+}
+
+function RunQuery(query_data) {
+    lastQuery = query_data;
+    var str_query = JSON.stringify(query_data);
+    if (Modernizr.localstorage) {
+        localStorage.setItem("last_query", str_query);
+    }
+    var start = new Date().getTime();  
+    $.ajax({ url: "/Search/", method: "POST", contentType: "application/json", data: str_query}).done(function(d) {
+            /*
+            var mismatch = (d.version != clientVersion);
+            if (mismatch) {
+            }
+            */
+            if (d.errors.length > 0) {
+                $('#resultHeader').text(d.errors[0]);
+            } else {
+                /*
+                $('#queryModal .progress').removeClass('active');
+                $('#queryModal .progress-bar').addClass('progress-bar-warning');
+                $('#queryModal #queryResult').html("Search found 0 results.");
+                $('#queryModal #cancel-query-btn').html("Close");
+                */
+
+                var end = new Date().getTime();
+                $('#resultHeader').text("" + d.count + " results (" + ((end - start) / 1000).toFixed(1) + " seconds)");
+                LoadResults(d);
+            }
+    }).fail(function() {
+        /*
+        $('#queryModal .progress-bar').addClass('progress-bar-danger');
+        $('#queryModal .progress').removeClass('active');
+        $('#queryModal #queryResult').html("Search failed");
+        $('#queryModal #cancel-query-btn').html("Close");
+        */ 
+        $('#resultHeader').text("Search failed.");
+    });
+}
+
+function UIInit() {
+    $('#pageSelection').bootpag({total: 0});
+    $('#searchSubmit').on('click', function(event) {
+        RunQuery(ParseQuery());
+    });
+}
+
+$(document).ready(function(){
+    UIInit();
+});
